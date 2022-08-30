b4cee1d739af51d85967346c792cd459b20b40b6
jingof <jofanson13@gmail.com>
2022-08-30 19:08:10 +0200

    first working commit

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'r478gk08ub';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/prod`
+
+export const authConfig = {
+  domain: 'dev-1uz7-kfb.us.auth0.com',
+  clientId: 'LvIX1yERBYaayarsF30ciMoJ3208q4F8',
+  callbackUrl: 'http://localhost:3000/callback'
+};
